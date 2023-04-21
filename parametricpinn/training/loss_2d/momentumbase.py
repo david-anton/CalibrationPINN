5be10e8a5b8f1130756eb7b9124cea311eb2a702
David Anton <d.anton@tu-braunschweig.de>
2023-04-21 10:58:59 +0000

    Start implementing loss funcs 2D. Not yet tested.

--- parametricpinn/training/loss_2d/momentumbase.py
+++ parametricpinn/training/loss_2d/momentumbase.py
@@ -5,76 +5,122 @@ from torch.autograd.functional import jacobian
 
 from parametricpinn.types import Module, Tensor
 
-StressFunc: TypeAlias = Callable[[Module, Tensor, Tensor, Tensor], Tensor]
-MomentumFunc: TypeAlias = Callable[[Module, Tensor, Tensor, Tensor, Tensor], Tensor]
-TractionFunc: TypeAlias = Callable[[Module, Tensor, Tensor, Tensor, Tensor], Tensor]
+StressFunc: TypeAlias = Callable[[Module, Tensor, Tensor], Tensor]
+MomentumFunc: TypeAlias = Callable[[Module, Tensor, Tensor, Tensor], Tensor]
+TractionFunc: TypeAlias = Callable[[Module, Tensor, Tensor, Tensor], Tensor]
 
 
 def _momentum_equation_func(stress_func: StressFunc) -> MomentumFunc:
     def momentum_equation_func(
-        ansatz: Module, x_coor: Tensor, x_E: Tensor, x_nu: Tensor, volume_force: Tensor
+        ansatz: Module, x_coor: Tensor, x_param: Tensor, volume_force: Tensor
     ) -> Tensor:
         return (
-            _divergence_stress_func(ansatz, x_coor, x_E, x_nu, stress_func)
-            + volume_force
+            _divergence_stress_func(ansatz, x_coor, x_param, stress_func) + volume_force
         )
 
     return momentum_equation_func
 
+
 def _traction_func(stress_func: StressFunc) -> TractionFunc:
     def traction_func(
-        ansatz: Module, x_coor: Tensor, x_E: Tensor, x_nu: Tensor, normal: Tensor
+        ansatz: Module, x_coor: Tensor, x_param: Tensor, normal: Tensor
     ) -> Tensor:
-        stress = stress_func(ansatz, x_coor, x_E, x_nu)
+        stress = stress_func(ansatz, x_coor, x_param)
         return torch.matmul(stress, normal)
 
     return traction_func
 
 
 def _divergence_stress_func(
-    ansatz: Module, x_coor: Tensor, x_E: Tensor, x_nu: Tensor, stress_func: StressFunc
+    ansatz: Module, x_coor: Tensor, x_param: Tensor, stress_func: StressFunc
 ) -> Tensor:
-    def jacobian_voigt_stress(x_coor: Tensor) -> Tensor:
-        return jacobian(
-            voigt_stress_func,
-            (x_coor),
-            create_graph=True,
-            strict=True,
-            strategy="reverse-mode",
-        )
-
-    def voigt_stress_func(x_coor: Tensor) -> Tensor:
-        stress = stress_func(ansatz, x_coor, x_E, x_nu)
-        return torch.tensor([stress[0, 0], stress[1, 1], stress[0, 1]])
-
-    jac_voigt_stress = jacobian_voigt_stress(x_coor)
-    sigxx_x = jac_voigt_stress[0, 0]
-    sigxy_y = jac_voigt_stress[2, 1]
-    sigyx_x = jac_voigt_stress[2, 0]
-    sigyy_y = jac_voigt_stress[1, 1]
-    return torch.tensor([sigxx_x + sigxy_y, sigyx_x + sigyy_y])
+    stress_xx_func = lambda x, y: stress_func(ansatz, torch.concat((x, y)), x_param)[
+        :, 0, 0
+    ].view((-1, 1))
+    stress_yy_func = lambda x, y: stress_func(ansatz, torch.concat((x, y)), x_param)[
+        :, 1, 1
+    ].view((-1, 1))
+    stress_xy_func = lambda x, y: stress_func(ansatz, torch.concat((x, y)), x_param)[
+        :, 0, 1
+    ].view((-1, 1))
+    sigxx_x = torch.autograd.grad(
+        stress_xx_func,
+        x,
+        grad_outputs=torch.ones_like(u),
+        retain_graph=True,
+        create_graph=True,
+        is_grads_batched=True,
+    )[0]
 
 
-def _strain_func(ansatz: Module, x_coor: Tensor, x_E: Tensor, x_nu: Tensor) -> Tensor:
-    jac_u = _jacobian_displacement_func(ansatz, x_coor, x_E, x_nu)
-    return 1 / 2 * (jac_u + torch.transpose(jac_u, 0, 1))
+# def _divergence_stress_func(
+#     ansatz: Module, x_coor: Tensor, x_param: Tensor, stress_func: StressFunc
+# ) -> Tensor:
+#     def jacobian_voigt_stress(x_coor: Tensor) -> Tensor:
+#         return jacobian(
+#             voigt_stress_func,
+#             (x_coor),
+#             create_graph=True,
+#             strict=True,
+#             vectorize=True,
+#             strategy="reverse-mode",
+#         )
+#         # return jacobian(
+#         #     voigt_stress_func,
+#         #     (x_coor),
+#         #     create_graph=True,
+#         #     strict=True,
+#         #     vectorize=True,
+#         #     strategy="reverse-mode",
+#         # )
+
+#     def voigt_stress_func(x_coor: Tensor) -> Tensor:
+#         stress = stress_func(ansatz, x_coor, x_param)
+#         return torch.transpose(
+#             torch.vstack((stress[:, 0, 0], stress[:, 1, 1], stress[:, 0, 1])), 0, 1
+#         )
+#         # return torch.tensor([stress[0, 0], stress[1, 1], stress[0, 1]])
+
+#     jac_voigt_stress = jacobian_voigt_stress(x_coor)
+#     sigxx_x = jac_voigt_stress[:, 0, 0]
+#     sigxy_y = jac_voigt_stress[:, 2, 1]
+#     sigyx_x = jac_voigt_stress[:, 2, 0]
+#     sigyy_y = jac_voigt_stress[:, 1, 1]
+#     return torch.transpose(torch.vstack((sigxx_x + sigxy_y, sigyx_x + sigyy_y)), 0, 1)
+#     # sigxx_x = jac_voigt_stress[0, 0]
+#     # sigxy_y = jac_voigt_stress[2, 1]
+#     # sigyx_x = jac_voigt_stress[2, 0]
+#     # sigyy_y = jac_voigt_stress[1, 1]
+#     # return torch.tensor([sigxx_x + sigxy_y, sigyx_x + sigyy_y])
+
+
+def _strain_func(ansatz: Module, x_coor: Tensor, x_param: Tensor) -> Tensor:
+    jac_u = _jacobian_displacement_func(ansatz, x_coor, x_param)
+    return 1 / 2 * (jac_u + torch.transpose(jac_u, 1, 2))
+    # return 1 / 2 * (jac_u + torch.transpose(jac_u, 0, 1))
 
 
 def _jacobian_displacement_func(
-    ansatz: Module, x_coor: Tensor, x_E: Tensor, x_nu: Tensor
+    ansatz: Module, x_coor: Tensor, x_param: Tensor
 ) -> Tensor:
-    displacement_func = lambda x_coor: _displacement_func(ansatz, x_coor, x_E, x_nu)
+    displacement_func = lambda x_coor: _displacement_func(ansatz, x_coor, x_param)
     return jacobian(
         displacement_func,
         (x_coor),
         create_graph=True,
         strict=True,
+        vectorize=True,
         strategy="reverse-mode",
-    )[0]
+    )
+    # return jacobian(
+    #     displacement_func,
+    #     (x_coor),
+    #     create_graph=True,
+    #     strict=True,
+    #     strategy="reverse-mode",
+    # )
 
 
-def _displacement_func(
-    ansatz: Module, x_coor: Tensor, x_E: Tensor, x_nu: Tensor
-) -> Tensor:
-    x = torch.concat((x_coor, x_E, x_nu), dim=0)
+def _displacement_func(ansatz: Module, x_coor: Tensor, x_param: Tensor) -> Tensor:
+    x = torch.concat((x_coor, x_param), dim=0)
     return ansatz(x)
