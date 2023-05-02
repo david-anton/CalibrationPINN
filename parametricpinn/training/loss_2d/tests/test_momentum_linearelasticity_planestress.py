1b9d356301c2d17a816cb4d59723e2bf04149ac2
David Anton <d.anton@tu-braunschweig.de>
2023-05-02 12:35:53 +0000

    Add autograd solution to loss 1D

--- parametricpinn/training/loss_2d/tests/test_momentum_linearelasticity_planestress.py
+++ parametricpinn/training/loss_2d/tests/test_momentum_linearelasticity_planestress.py
@@ -1,17 +1,17 @@
-import torch
-import torch.nn as nn
+# import torch
+# import torch.nn as nn
 
-from parametricpinn.types import Tensor
+# from parametricpinn.types import Tensor
 
 
-class FakeAnsatzPlaneStress(nn.Module):
-    def __init__(self) -> None:
-        super().__init__()
-        self._constants = torch.tensor([8 / 39, -44 / 39])
+# class FakeAnsatzPlaneStress(nn.Module):
+#     def __init__(self) -> None:
+#         super().__init__()
+#         self._constants = torch.tensor([8 / 39, -44 / 39])
 
-    def forward(self, x: Tensor) -> Tensor:
-        coor_x = x[0]
-        coor_y = x[1]
-        return self._constants * torch.tensor(
-            [coor_x**2 * coor_y, coor_x * coor_y**2]
-        )
+#     def forward(self, x: Tensor) -> Tensor:
+#         coor_x = x[0]
+#         coor_y = x[1]
+#         return self._constants * torch.tensor(
+#             [coor_x**2 * coor_y, coor_x * coor_y**2]
+#         )
