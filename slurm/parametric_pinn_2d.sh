f6f268b21af21980569e912486a2770cf3b20d9e
David Anton <d.anton@tu-braunschweig.de>
2023-05-17 08:04:43 +0000

    Make container.def fr development

--- slurm/parametric_pinn_2d.sh
+++ slurm/parametric_pinn_2d.sh
@@ -10,4 +10,5 @@
 
 SCRIPT=parametric_pinn_2d.py
 
-srun singularity exec --cleanenv --nv --no--mount /.bashrc,/.bash_profile --bind output:/data/output,input:/data/input parametricpinn.sif python3 /data/app/${SCRIPT}
+srun singularity exec --cleanenv --nv --no--mount /.bashrc,/.bash_profile parametricpinn.sif python3 app/${SCRIPT}
+##srun singularity exec --cleanenv --nv --no--mount /.bashrc,/.bash_profile --bind output:/data/output,input:/data/input parametricpinn.sif python3 /data/app/${SCRIPT}
