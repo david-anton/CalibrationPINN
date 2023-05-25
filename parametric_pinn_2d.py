8b7f3bace6461bcafac4a8e5a054647e9504f887
David Anton <d.anton@tu-braunschweig.de>
2023-05-25 12:49:47 +0000

    Debugging

--- parametric_pinn_2d.py
+++ parametric_pinn_2d.py
@@ -65,7 +65,7 @@ num_samples_valid = 32
 valid_interval = 1
 num_points_valid = 1024
 batch_size_valid = num_samples_valid
-fem_mesh_resolution = 10  # 0.1
+fem_mesh_resolution = 1  # 0.1
 # Output
 current_date = date.today().strftime("%Y%m%d")
 output_subdir = (
