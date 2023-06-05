77032aeff7bf2833cb89257444d8bf7e4684a923
David Anton <d.anton@tu-braunschweig.de>
2023-06-05 13:51:53 +0200

    Refactoring

--- parametricpinn/postprocessing/plot/__init__.py
+++ parametricpinn/postprocessing/plot/__init__.py
@@ -1,9 +1,15 @@
 from .plot_displacements_1d import DisplacementsPlotterConfig1D, plot_displacements_1D
+from .plot_displacements_pwh import (
+    DisplacementsPlotterConfigPWH,
+    plot_displacements_pwh,
+)
 from .plot_history import HistoryPlotterConfig, plot_loss_history, plot_valid_history
 
 __all__ = [
     "DisplacementsPlotterConfig1D",
     "plot_displacements_1D",
+    "DisplacementsPlotterConfigPWH",
+    "plot_displacements_pwh",
     "HistoryPlotterConfig",
     "plot_loss_history",
     "plot_valid_history",
