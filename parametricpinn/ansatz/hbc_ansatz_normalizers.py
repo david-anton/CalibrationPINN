e239f784b1cbc81069b79be71f8483a559735179
David Anton <d.anton@tu-braunschweig.de>
2023-06-07 12:18:09 +0000

    Try to find bugs

--- parametricpinn/ansatz/hbc_ansatz_normalizers.py
+++ parametricpinn/ansatz/hbc_ansatz_normalizers.py
@@ -7,6 +7,13 @@ class HBCAnsatzNormalizer(nn.Module):
     def __init__(self, min_values: Tensor, max_values: Tensor) -> None:
         super().__init__()
         self._value_ranges = max_values - min_values
+        print("################################")
+        print("################################")
+        print(f"min_values {min_values.get_device()}")
+        print(f"max_values {max_values.get_device()}")
+        print(f"value_ranges {self._value_ranges.get_device()}")
+        print("################################")
+        print("################################")
 
     def forward(self, x: Tensor) -> Tensor:
         return x / self._value_ranges
@@ -16,6 +23,13 @@ class HBCAnsatzRenormalizer(nn.Module):
     def __init__(self, min_values: Tensor, max_values: Tensor) -> None:
         super().__init__()
         self._value_ranges = max_values - min_values
+        print("################################")
+        print("################################")
+        print(f"min_values {min_values.get_device()}")
+        print(f"max_values {max_values.get_device()}")
+        print(f"value_ranges {self._value_ranges.get_device()}")
+        print("################################")
+        print("################################")
 
     def forward(self, normalized_x: Tensor) -> Tensor:
         return normalized_x * self._value_ranges
