a900bef2f80b1ef6f3086035d4c3698f03e6c41f
David Anton <d.anton@tu-braunschweig.de>
2023-06-08 08:52:10 +0000

    Set up simulation

--- pinn_2d.py
+++ pinn_2d.py
@@ -59,8 +59,8 @@ batch_size_train = 1
 num_epochs = 2000
 loss_metric = torch.nn.MSELoss(reduction="mean")
 # Validation
-regenerate_valid_data = False
-input_subdir_valid = "20230607_validation_data_E_210000_nu_03"
+regenerate_valid_data = True
+input_subdir_valid = "20230608_validation_data_E_210000_nu_03"
 num_samples_valid = 1
 valid_interval = 1
 num_points_valid = 1024
