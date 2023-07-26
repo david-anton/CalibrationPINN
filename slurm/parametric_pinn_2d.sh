82f8f04061c9077847d1c01fe983aaa14fa351e8
David Anton <d.anton@tu-braunschweig.de>
2023-07-26 11:23:25 +0000

    Run simulation on gpu_irmb_half

--- slurm/parametric_pinn_2d.sh
+++ slurm/parametric_pinn_2d.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --partition=gpu_irmb_full
+#SBATCH --partition=gpu_irmb_half
 #SBATCH --nodes=1
 #SBATCH --time=96:00:00
 #SBATCH --job-name=PPINN2D
 #SBATCH --ntasks-per-node=1
-#SBATCH --gres=gpu:ampere:1
+#SBATCH --gres=gpu:3g.40gb:1
 
 ## Build command
 ## singularity build --fakeroot --force parametricpinn.sif app/.devcontainer/container.def
