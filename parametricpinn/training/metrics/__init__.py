a2219401e078238bcc05d97151f14cdd8122534b
David Anton <d.anton@tu-braunschweig.de>
2023-07-26 11:52:41 +0000

    Add subpackage for metrics

---
+++ parametricpinn/training/metrics/__init__.py
@@ -0,0 +1,17 @@
+from .metrics import (
+    l2_norm,
+    mean_absolute_error,
+    mean_absolute_relative_error,
+    mean_relative_error,
+    mean_squared_error,
+    relative_l2_norm,
+)
+
+__all__ = [
+    "l2_norm",
+    "mean_absolute_error",
+    "mean_absolute_relative_error",
+    "mean_relative_error",
+    "mean_squared_error",
+    "relative_l2_norm",
+]
