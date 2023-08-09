8b6280816c93bc5c486474432321231593e48300
David Anton <d.anton@tu-braunschweig.de>
2023-08-09 08:16:50 +0000

    Add slurm scripts

--- slurm/parametric_pinn_1d.sh
+++ slurm/parametric_pinn_1d.sh
@@ -2,14 +2,14 @@
 #SBATCH --partition=gpu_irmb_full
 #SBATCH --nodes=1
 #SBATCH --time=96:00:00
-#SBATCH --job-name=PPINN2D
+#SBATCH --job-name=PPINN1D
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:ampere:1
 
 ## Build command
 ## singularity build --fakeroot --force parametricpinn.sif app/.devcontainer/container.def
 
-SCRIPT=parametric_pinn_2d.py
+SCRIPT=parametric_pinn_1d.py
 
 srun singularity exec \
  --cleanenv \
