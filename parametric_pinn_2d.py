37d65cab5a5ddf12903db00058fcb270102e6e11
David Anton <d.anton@tu-braunschweig.de>
2023-09-06 06:31:59 +0000

    Set up test simulations

--- parametric_pinn_2d.py
+++ parametric_pinn_2d.py
@@ -48,7 +48,7 @@ from parametricpinn.training.training_standard_2d import (
 from parametricpinn.types import Tensor
 
 ### Configuration
-retrain_parametric_pinn = True
+retrain_parametric_pinn = False
 # Set up
 material_model = "plane stress"
 edge_length = 100.0
@@ -86,7 +86,7 @@ use_hamiltonian = True
 use_efficient_nuts = True
 # Output
 current_date = date.today().strftime("%Y%m%d")
-output_date = current_date
+output_date = 20230901
 output_subdirectory = f"{output_date}_parametric_pinn_E_180k_240k_nu_02_04_samples_32_col_64_bc_32_full_batch_neurons_4_32"
 output_subdirectory_preprocessing = f"{output_date}_preprocessing"
 save_metadata = True
