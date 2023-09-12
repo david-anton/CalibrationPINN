70cd527ebd55fbabae7e8da73e159d92c5d8edca
David Anton <d.anton@tu-braunschweig.de>
2023-09-12 11:16:32 +0000

    Add slurm script for calibration paper

---
+++ slurm/parametric_pinn_2d_calibrationpap.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --partition=gpu_irmb_full
+#SBATCH --nodes=1
+#SBATCH --time=96:00:00
+#SBATCH --job-name=PPINN2D
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:ampere:1
+
+## Build command
+## singularity build --fakeroot --force parametricpinn.sif app/.devcontainer/container.def
+
+SCRIPT=parametric_pinn_2d_calibrationpaper.py
+
+srun singularity exec \
+ --cleanenv \
+ --env CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES \
+ --nv \
+ parametricpinn.sif \
+ python3 /home/davanton/development/ParametricPINN/app/${SCRIPT}
+
+
