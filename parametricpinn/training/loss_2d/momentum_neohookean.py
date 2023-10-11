6766418b84ba2c9a21168ff51041c6d7de0d552a
David Anton <d.anton@tu-braunschweig.de>
2023-10-11 09:24:27 +0000

    Bug fix in test

--- parametricpinn/training/loss_2d/momentum_neohookean.py
+++ parametricpinn/training/loss_2d/momentum_neohookean.py
@@ -102,7 +102,7 @@ def _first_piola_stress_tensor(
     free_energy_func = lambda deformation_gradient: _free_energy_func(
         deformation_gradient, x_param
     )
-    stress = free_energy_func(F)
+    stress = torch.full((2,2), free_energy_func(F))
     # stress = grad(free_energy_func)(F)
     return stress
 
