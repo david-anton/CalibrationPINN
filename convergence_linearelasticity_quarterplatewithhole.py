84905f482037c4d26055e532caa22a3487c0753c
David Anton <d.anton@tu-braunschweig.de>
2023-11-04 12:47:14 +0000

    set up simulation

--- convergence_linearelasticity_quarterplatewithhole.py
+++ convergence_linearelasticity_quarterplatewithhole.py
@@ -43,7 +43,7 @@ poissons_ratio = 0.3
 # FEM
 fem_element_family = "Lagrange"
 fem_element_degree = 2
-fem_element_size_reference = 0.1
+fem_element_size_reference = 0.2
 fem_element_size_largest = 1.6
 fem_reduction_factor = 1 / 2
 fem_element_size_tests = (
