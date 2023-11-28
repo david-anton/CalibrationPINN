116d3a0b375c6323b5b3f40de0df6e9e20b9873b
David Anton <d.anton@tu-braunschweig.de>
2023-11-28 10:25:08 +0000

    Refactoring

--- parametricpinn/gps/__init__.py
+++ parametricpinn/gps/__init__.py
@@ -1,4 +1,5 @@
 from .base import GaussianProcess
+from .independentmultioutput_gp import IndependentMultiOutputGP
 from .zeromean_scaledrbfkernel_gp import ZeroMeanScaledRBFKernelGP
 
-__all__ = ["GaussianProcess", "ZeroMeanScaledRBFKernelGP"]
+__all__ = ["GaussianProcess", "IndependentMultiOutputGP", "ZeroMeanScaledRBFKernelGP"]
