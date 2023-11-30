f693d4d0280054e00594dac1f022c20236b29a57
David Anton <d.anton@tu-braunschweig.de>
2023-11-30 06:58:58 +0000

    Set up cuda test

---
+++ slurm/cuda_test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --partition=gpu_irmb
+#SBATCH --nodes=1
+#SBATCH --time=1:00:00
+#SBATCH --job-name=Test
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:ampere:1
+
+## Build command
+## singularity build --fakeroot --force parametricpinn.sif app/.devcontainer/container_conda.def
+
+SCRIPT=cuda_test.py
+
+srun singularity run \
+ --cleanenv \
+ --env CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES \
+ --nv \
+ --nvccli \
+ parametricpinn.sif \
+ python3 /home/davanton/development/ParametricPINN/app/${SCRIPT}
+
+
