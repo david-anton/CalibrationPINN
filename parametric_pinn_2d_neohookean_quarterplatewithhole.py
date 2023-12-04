157812afc0cb898fe5347dc8a80eb395692027f5
David Anton <d.anton@tu-braunschweig.de>
2023-12-04 11:28:08 +0000

    Bug fix

--- parametric_pinn_2d_neohookean_quarterplatewithhole.py
+++ parametric_pinn_2d_neohookean_quarterplatewithhole.py
@@ -89,7 +89,7 @@ weight_symmetry_bc_loss = 1.0
 weight_traction_bc_loss = 1.0
 # Validation
 regenerate_valid_data = False
-input_subdir_valid = "20231128_validation_data_neohookean_E_1000_3000_nu_02_04_edge_100_radius_10_traction_100_elementsize_01"
+input_subdir_valid = "20231128_validation_data_neohookean_E_1000_3000_nu_02_04_edge_100_radius_10_traction_100_elementsize_01_BC_complete"
 num_samples_valid = 32
 validation_interval = 1
 num_points_valid = 1024
