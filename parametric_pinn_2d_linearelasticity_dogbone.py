623080f757bcd87efb6e1dcbead186d8b5d1d74e
David Anton <d.anton@tu-braunschweig.de>
2023-12-04 14:40:49 +0000

    Dogbone forward simulation

--- parametric_pinn_2d_linearelasticity_dogbone.py
+++ parametric_pinn_2d_linearelasticity_dogbone.py
@@ -79,10 +79,10 @@ layer_sizes = [4, 32, 32, 32, 32, 2]
 distance_function = "normalized linear"
 # Training
 num_samples_per_parameter = 1  # 32
-num_collocation_points = 4096  # 128
+num_collocation_points =   16384# 128
 number_points_per_bc = 1024  # 32
 training_batch_size = num_samples_per_parameter**2
-number_training_epochs = 500  # 20000
+number_training_epochs = 20000
 weight_pde_loss = 1.0
 weight_traction_bc_loss = 1.0
 # Validation
@@ -90,7 +90,7 @@ regenerate_valid_data = True  # False
 input_subdir_valid = "20231204_validation_data_linearelasticity_dogbone_E_210k_nu_03_elementsize_01"  # "20231201_validation_data_linearelasticity_dogbone_E_180k_240k_nu_02_04_elementsize_01"
 num_samples_valid = 1  # 32
 validation_interval = 1
-num_points_valid = 1024
+num_points_valid = 4096 #1024
 batch_size_valid = num_samples_valid
 # Calibration
 input_subdir_calibration = "20231124_experimental_dic_data_dogbone"
@@ -106,7 +106,7 @@ fem_element_size = 0.1
 # Output
 current_date = date.today().strftime("%Y%m%d")
 output_date = current_date
-output_subdirectory = f"{output_date}_parametric_pinn_linearelasticity_dogbone_E_210k_nu_03_col_4096_bc_128_neurons_4_32"  # f"{output_date}_parametric_pinn_linearelasticity_dogbone_E_180k_240k_nu_02_04_samples_32_col_128_bc_32_neurons_4_32"
+output_subdirectory = f"{output_date}_parametric_pinn_linearelasticity_dogbone_E_210k_nu_03_col_16384_bc_1024_neurons_4_32"  # f"{output_date}_parametric_pinn_linearelasticity_dogbone_E_180k_240k_nu_02_04_samples_32_col_128_bc_32_neurons_4_32"
 output_subdirectory_preprocessing = f"{output_date}_preprocessing"
 save_metadata = True
 
