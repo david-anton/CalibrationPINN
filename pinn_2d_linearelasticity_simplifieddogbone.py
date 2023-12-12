1fe39a2f7262cd8fa352fc14158f64dd94595d05
David Anton <d.anton@tu-braunschweig.de>
2023-12-12 12:37:48 +0000

    Bug fix

--- pinn_2d_linearelasticity_simplifieddogbone.py
+++ pinn_2d_linearelasticity_simplifieddogbone.py
@@ -67,8 +67,8 @@ weight_traction_bc_loss = 1.0
 weight_free_traction_bc_loss=1.0
 weight_dirichlet_bc_loss=0.0
 # Validation
-regenerate_valid_data = False
-input_subdir_valid = "20231212_validation_data_linearelasticity_simplifieddogbone_E_210k_nu_03_elementsize_01"
+regenerate_valid_data = True
+input_subdir_valid = "20231212_validation_data_linearelasticity_simplifieddogbone_E_210k_nu_03_elementsize_01_rectangle"
 num_samples_valid = 1
 validation_interval = 1
 num_points_valid = 1024
@@ -80,7 +80,7 @@ fem_element_size = 0.1
 # Output
 current_date = date.today().strftime("%Y%m%d")
 output_date = current_date
-output_subdirectory = f"{output_date}_pinn_linearelasticity_simplifieddogbone_E_210k_nu_03_col_4096_bc_128_neurons_4_32_overlap_30" 
+output_subdirectory = f"{output_date}_pinn_linearelasticity_simplifieddogbone_E_210k_nu_03_col_4096_bc_128_neurons_4_32_rectangle" 
 output_subdirectory_preprocessing = f"{output_date}_preprocessing"
 save_metadata = True
 
