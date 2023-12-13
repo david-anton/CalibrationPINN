80ec7bc808ccc76fa2af4e626b26c9df2c0772fa
David Anton <d.anton@tu-braunschweig.de>
2023-12-13 10:47:59 +0000

    Bug fix

--- pinn_2d_linearelasticity_simplifieddogbone.py
+++ pinn_2d_linearelasticity_simplifieddogbone.py
@@ -68,7 +68,7 @@ weight_free_traction_bc_loss=1.0
 weight_dirichlet_bc_loss=0.0
 # Validation
 regenerate_valid_data = True
-input_subdir_valid = "20231212_validation_data_linearelasticity_simplifieddogbone_E_210k_nu_03_elementsize_01_plate"
+input_subdir_valid = "20231213_validation_data_linearelasticity_simplifieddogbone_E_210k_nu_03_elementsize_01_plate"
 num_samples_valid = 1
 validation_interval = 1
 num_points_valid = 1024
