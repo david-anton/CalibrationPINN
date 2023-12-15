4f9f354a6c6fc54f98d3822e1054aaae106c2220
David Anton <d.anton@tu-braunschweig.de>
2023-12-15 07:33:27 +0000

    Simulation

--- pinn_2d_linearelasticity_simplifieddogbone.py
+++ pinn_2d_linearelasticity_simplifieddogbone.py
@@ -63,11 +63,11 @@ bcs_overlap_angle_distance = 0
 bcs_overlap_distance_left = 0
 bcs_overlap_distance_right = 0
 training_batch_size = num_samples_per_parameter**2
-number_training_epochs = 5000
+number_training_epochs = 2000
 weight_pde_loss = 1.0
 weight_traction_bc_loss = 1.0
 weight_free_traction_bc_loss=1.0
-weight_dirichlet_bc_loss=1.0
+weight_dirichlet_bc_loss=100.0
 weight_energy_loss=0.0
 weight_symmetry_loss=0.0
 # Validation
@@ -84,7 +84,7 @@ fem_element_size = 0.1
 # Output
 current_date = date.today().strftime("%Y%m%d")
 output_date = current_date
-output_subdirectory = f"{output_date}_pinn_linearelasticity_simplifieddogbone_E_210k_nu_03_col_4096_bc_128_neurons_4_32_uniform_without_HBC" 
+output_subdirectory = f"{output_date}_pinn_linearelasticity_simplifieddogbone_E_210k_nu_03_col_4096_bc_128_neurons_4_32_uniform_without_HBC_dirichlet_100" 
 output_subdirectory_preprocessing = f"{output_date}_preprocessing"
 save_metadata = True
 
