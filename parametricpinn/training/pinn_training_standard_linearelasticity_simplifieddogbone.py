4d31340ae4b070f34737889eabe8928aacf3d0af
David Anton <d.anton@tu-braunschweig.de>
2023-12-18 08:49:51 +0000

    Simulation

--- parametricpinn/training/pinn_training_standard_linearelasticity_simplifieddogbone.py
+++ parametricpinn/training/pinn_training_standard_linearelasticity_simplifieddogbone.py
@@ -401,8 +401,8 @@ def train_parametric_pinn(train_config: TrainingConfiguration) -> None:
     train_batches = iter(train_dataloader)
     for batch_collocation, batch_traction_bc in train_batches:
         fig_collocation, ax_collocation = plt.subplots()
-        fig_collocation.set_figheight(4)
-        fig_collocation.set_figwidth(14)
+        fig_collocation.set_figheight(6)
+        fig_collocation.set_figwidth(4 + 1)
         x_collocation = batch_collocation.x_coor[:, 0]
         y_collocation = batch_collocation.x_coor[:, 1]
         ax_collocation.scatter(x_collocation, y_collocation, edgecolors="none")
@@ -417,8 +417,8 @@ def train_parametric_pinn(train_config: TrainingConfiguration) -> None:
         )
 
         fig_bc, ax_bc = plt.subplots()
-        fig_bc.set_figheight(4)
-        fig_bc.set_figwidth(16)
+        fig_bc.set_figheight(6)
+        fig_bc.set_figwidth(4 + 1)
         x_bc = batch_traction_bc.x_coor[:num_points_per_bc, 0]
         y_bc = batch_traction_bc.x_coor[:num_points_per_bc, 1]
         x_bc_free = batch_traction_bc.x_coor[num_points_per_bc:, 0]
