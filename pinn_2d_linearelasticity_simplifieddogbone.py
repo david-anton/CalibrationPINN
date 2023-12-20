ec8955301de16d47264d42af344e56fc1a009b66
David Anton <d.anton@tu-braunschweig.de>
2023-12-20 10:47:54 +0000

    Simulation

--- pinn_2d_linearelasticity_simplifieddogbone.py
+++ pinn_2d_linearelasticity_simplifieddogbone.py
@@ -53,7 +53,7 @@ max_youngs_modulus = min_youngs_modulus
 min_poissons_ratio = 0.3
 max_poissons_ratio = min_poissons_ratio
 # Network
-layer_sizes = [4, 64, 64, 64, 64, 64, 64, 2]
+layer_sizes = [4, 128, 128, 128, 128, 128, 128, 2] #[4, 64, 64, 64, 64, 64, 64, 2]
 # Ansatz
 distance_function = "normalized linear"
 # Training
@@ -86,7 +86,7 @@ fem_element_size = 0.1
 # Output
 current_date = date.today().strftime("%Y%m%d")
 output_date = current_date
-output_subdirectory = f"{output_date}_pinn_linearelasticity_simplifieddogbone_E_210k_nu_03_col_8192_bc_256_neurons_6_64_symmetry_{weight_symmetry_loss}"
+output_subdirectory = f"{output_date}_pinn_linearelasticity_simplifieddogbone_E_210k_nu_03_col_8192_bc_256_neurons_6_128_symmetry_{weight_symmetry_loss}"
 output_subdirectory_preprocessing = f"{output_date}_preprocessing"
 save_metadata = True
 
