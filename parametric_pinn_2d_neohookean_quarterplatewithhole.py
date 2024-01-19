1f3fec74d724cb47083e957bfbd42802a29b650c
David Anton <d.anton@tu-braunschweig.de>
2024-01-19 13:56:01 +0000

    Set up test neohook

--- parametric_pinn_2d_neohookean_quarterplatewithhole.py
+++ parametric_pinn_2d_neohookean_quarterplatewithhole.py
@@ -75,8 +75,8 @@ volume_force_x = 0.0
 volume_force_y = 0.0
 min_bulk_modulus = 1000.0
 max_bulk_modulus = 2000.0
-min_rivlin_saunders_c_10 = 10.0
-max_rivlin_saunders_c_10 = 30.0
+min_rivlin_saunders_c_10 = 20.0
+max_rivlin_saunders_c_10 = 40.0
 # Network
 layer_sizes = [4, 64, 64, 64, 64, 2]
 # Ansatz
@@ -94,7 +94,7 @@ weight_stress_bc_loss = 1.0
 weight_traction_bc_loss = 1.0
 # Validation
 regenerate_valid_data = True
-input_subdir_valid = "20240119_validation_data_neohookean_quarterplatewithhole_K_1000_2000_c10_10_30_edge_100_radius_10_traction_10_elementsize_02"
+input_subdir_valid = "20240119_validation_data_neohookean_quarterplatewithhole_K_1000_2000_c10_20_40_edge_100_radius_10_traction_10_elementsize_02"
 num_samples_valid = 1  # 32
 validation_interval = 1
 num_points_valid = 1024
@@ -112,7 +112,7 @@ fem_element_size = 0.2
 # Output
 current_date = date.today().strftime("%Y%m%d")
 output_date = current_date
-output_subdirectory = f"{output_date}_parametric_pinn_neohookean_quarterplatewithhole_K_1000_2000_c10_10_30_col_128_bc_64_neurons_4_64_traction_10"
+output_subdirectory = f"{output_date}_parametric_pinn_neohookean_quarterplatewithhole_K_1000_2000_c10_20_40_col_128_bc_64_neurons_4_64_traction_10"
 output_subdirectory_preprocessing = f"{output_date}_preprocessing"
 save_metadata = True
 
