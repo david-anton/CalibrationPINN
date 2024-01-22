6284fef22f17c8fa9e99466f5f5577b98dfa0602
David Anton <d.anton@tu-braunschweig.de>
2024-01-22 15:50:23 +0000

    Calibration

--- parametric_pinn_2d_linearelasticity_simplifieddogbone_E_nu.py
+++ parametric_pinn_2d_linearelasticity_simplifieddogbone_E_nu.py
@@ -400,6 +400,7 @@ def calibration_step() -> None:
         full_displacements = torch.from_numpy(data[:, slice_displacements]).type(
             torch.float64
         )
+        print(f"Minimal full coordinates: {torch.amin(full_coordinates)}")
         random_indices = torch.randint(low=0, high=size_data, size=(num_data_points,))
         coordinates = full_coordinates[random_indices, :].to(device)
         displacements = full_displacements[random_indices, :].to(device)
