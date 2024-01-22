0bebfc33d68f3fae04d7058a85d15c748c871bcd
David Anton <d.anton@tu-braunschweig.de>
2024-01-22 13:28:48 +0000

    Calibration

--- parametric_pinn_2d_linearelasticity_simplifieddogbone_E_nu.py
+++ parametric_pinn_2d_linearelasticity_simplifieddogbone_E_nu.py
@@ -650,7 +650,7 @@ def calibration_step() -> None:
     )
     mean_displacements = torch.mean(torch.absolute(displacements), dim=0)
     print(f"1 / mean displacements: {1 / mean_displacements}")
-    residual_weights = torch.tensor([1e2, 1e3])
+    residual_weights = torch.tensor([1e2, 5e3])
     print(f"Used residual weights: {residual_weights}")
 
     least_squares_config = LeastSquaresConfig(
