6068a48a7e24ef315cbab949a82d40140c5f4713
David Anton <d.anton@tu-braunschweig.de>
2024-01-26 08:21:18 +0000

    Test

--- parametric_pinn_2d_neohookean_platewithhole.py
+++ parametric_pinn_2d_neohookean_platewithhole.py
@@ -69,14 +69,14 @@ retrain_parametric_pinn = True
 # Set up
 num_material_parameters = 2
 edge_length = 100.0
-radius = 20.0
-traction_left_x = -50.0
+radius = 10.0
+traction_left_x = -100.0
 traction_left_y = 0.0
 volume_force_x = 0.0
 volume_force_y = 0.0
 min_bulk_modulus = 1000.0
 max_bulk_modulus = 20000.0
-min_rivlin_saunders_c_10 = 100.0
+min_rivlin_saunders_c_10 = 200.0
 max_rivlin_saunders_c_10 = 2000.0
 # Network
 layer_sizes = [4, 64, 64, 64, 64, 2]
