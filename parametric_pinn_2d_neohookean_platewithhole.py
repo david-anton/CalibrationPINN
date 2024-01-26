b55082b15f82388115b5d23bbe9f78d27c0b5860
David Anton <d.anton@tu-braunschweig.de>
2024-01-26 08:55:52 +0000

    Test

--- parametric_pinn_2d_neohookean_platewithhole.py
+++ parametric_pinn_2d_neohookean_platewithhole.py
@@ -75,7 +75,7 @@ traction_left_y = 0.0
 volume_force_x = 0.0
 volume_force_y = 0.0
 min_bulk_modulus = 1000.0
-max_bulk_modulus = 20000.0
+max_bulk_modulus = 10000.0
 min_rivlin_saunders_c_10 = 200.0
 max_rivlin_saunders_c_10 = 2000.0
 # Network
