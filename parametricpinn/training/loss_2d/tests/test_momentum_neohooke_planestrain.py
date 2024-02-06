d4e2c10a5eb5393d6df12d111a41ffed54003362
David Anton <d.anton@tu-braunschweig.de>
2024-02-06 17:08:25 +0000

    Fix tests

--- parametricpinn/training/loss_2d/tests/test_momentum_neohooke_planestrain.py
+++ parametricpinn/training/loss_2d/tests/test_momentum_neohooke_planestrain.py
@@ -119,13 +119,14 @@ def _calculate_single_first_piola_kirchhoff_stress_tensor(
 
     # 2. Piola-Kirchoff stress tensor
     I = torch.eye(3)
+    C_inverse = torch.inverse(C)
     C_iso_inverse = torch.inverse(C_iso)
-    T = (J ** (1 / 3)) * param_K * (J - 1) * C_iso_inverse + 2 * (J ** (-2 / 3)) * (
-        param_c_10 * I - (1 / 3) * param_c_10 * I_C_iso * C_iso_inverse
-    )
+    T_vol = param_K / 2 * (J**2 - 1) * C_inverse
+    T_iso = 2 * param_c_10 * (J ** (-2 / 3)) * (I - (1 / 3) * I_C_iso * C_iso_inverse)
+    T = T_vol + T_iso
 
     # 1. Piola-Kirchoff stress tensor
-    P = F * T
+    P = torch.matmul(F, T)
     P_2D = P[0:2, 0:2]
     return P_2D
 
