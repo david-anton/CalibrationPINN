b4efed75d03d1b9cfd49a207512862fcfef50cd0
David Anton <d.anton@tu-braunschweig.de>
2024-02-29 07:44:21 +0000

    CALIBRATION

--- parametric_pinn_2d_neohooke_quarterplatewithhole.py
+++ parametric_pinn_2d_neohooke_quarterplatewithhole.py
@@ -102,7 +102,7 @@ fem_element_size = 0.2
 # Validation
 regenerate_valid_data = False
 input_subdir_valid = f"20240223_validation_data_neohooke_quarterplatewithhole_K_{int(min_bulk_modulus)}_{int(max_bulk_modulus)}_G_{int(min_shear_modulus)}_{int(max_shear_modulus)}_edge_{int(edge_length)}_radius_{int(radius)}_traction_{int(traction_left_x)}_elementsize_{fem_element_size}"
-num_samples_valid = 10  # 100
+num_samples_valid = 1  # 100
 validation_interval = 1
 num_points_valid = 1024
 batch_size_valid = num_samples_valid
@@ -426,7 +426,7 @@ def training_step() -> None:
 
 def calibration_step() -> None:
     print("Start calibration ...")
-    num_data_points = 256
+    num_data_points = 32  # 256
     std_noise = 5 * 1e-4
     num_test_cases = num_samples_valid
     prior_mean_bulk_modulus = mean_bulk_modulus
@@ -559,8 +559,8 @@ def calibration_step() -> None:
                 initial_parameters=initial_parameters,
                 num_iterations=int(1e4),
                 num_burn_in_iterations=int(5e3),
-                num_leabfrog_steps=256,
-                leapfrog_step_sizes=torch.tensor([1, 0.01], device=device),
+                num_leabfrog_steps=128,
+                leapfrog_step_sizes=torch.tensor([1, 0.1], device=device),
             )
             configs.append(config)
         return tuple(configs)
@@ -576,8 +576,8 @@ def calibration_step() -> None:
                 initial_parameters=initial_parameters,
                 num_iterations=int(1e4),
                 num_burn_in_iterations=int(1e4),
-                max_tree_depth=8,
-                leapfrog_step_sizes=torch.tensor([1, 0.01], device=device),
+                max_tree_depth=7,
+                leapfrog_step_sizes=torch.tensor([1, 0.1], device=device),
             )
             configs.append(config)
         return tuple(configs)
