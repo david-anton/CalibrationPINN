6c18b49a03d3a3a33e1a99385f63ef45ed0fb431
David Anton <d.anton@tu-braunschweig.de>
2024-03-19 14:53:00 +0000

    CALIBRATION

--- parametric_pinn_2d_linearelasticity_quarterplatewithhole.py
+++ parametric_pinn_2d_linearelasticity_quarterplatewithhole.py
@@ -112,7 +112,7 @@ validation_interval = 1
 num_points_valid = 1024
 batch_size_valid = num_samples_valid
 # Calibration
-use_q_likelihood = False
+use_q_likelihood = True
 use_least_squares = True
 use_random_walk_metropolis_hasting = True
 use_hamiltonian = False
@@ -459,8 +459,8 @@ def training_step() -> None:
 def calibration_step() -> None:
     print("Start calibration ...")
     num_test_cases = num_samples_valid
-    num_data_sets = 1
-    num_data_points = 128
+    num_data_sets = 64
+    num_data_points = 64
     std_noise = 5 * 1e-4
 
     initial_bulk_modulus = 160000.0
