7622966187b6039872638c06e87e9c155b8dcfa4
David Anton <d.anton@tu-braunschweig.de>
2024-03-19 12:44:58 +0000

    BUGFIX

--- parametric_pinn_2d_neohooke_quarterplatewithhole.py
+++ parametric_pinn_2d_neohooke_quarterplatewithhole.py
@@ -100,12 +100,12 @@ fem_element_size = 0.2
 # Validation
 regenerate_valid_data = False
 input_subdir_valid = f"20240305_validation_data_neohooke_quarterplatewithhole_K_{int(min_bulk_modulus)}_{int(max_bulk_modulus)}_G_{int(min_shear_modulus)}_{int(max_shear_modulus)}_edge_{int(edge_length)}_radius_{int(radius)}_traction_{int(traction_left_x)}_elementsize_{fem_element_size}"
-num_samples_valid = 100
+num_samples_valid = 1
 validation_interval = 1
 num_points_valid = 1024
 batch_size_valid = num_samples_valid
 # Calibration
-use_q_likelihood = True
+use_q_likelihood = False
 use_least_squares = True
 use_random_walk_metropolis_hasting = True
 use_hamiltonian = False
@@ -431,8 +431,8 @@ def training_step() -> None:
 def calibration_step() -> None:
     print("Start calibration ...")
     num_test_cases = num_samples_valid
-    num_data_sets = 64  # 1
-    num_data_points = 64  # 128
+    num_data_sets = 1
+    num_data_points = 128
     std_noise = 5 * 1e-4
 
     initial_bulk_modulus = 6000.0
@@ -507,7 +507,7 @@ def calibration_step() -> None:
         )
     else:
         output_subdir_calibration = os.path.join(
-            output_subdirectory, "calibration", "with_model_gps_error"
+            output_subdirectory, "calibration", "with_model_error_gps"
         )
     output_subdir_likelihoods = os.path.join(output_subdir_calibration, "likelihoods")
 
