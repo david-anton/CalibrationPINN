6aed2968ee161a03f742da53483fb9ae5b97b8dc
David Anton <d.anton@tu-braunschweig.de>
2024-03-25 13:04:55 +0000

    CALIBRATION

--- parametricpinn/calibration/bayesianinference/likelihoods/likelihoods.py
+++ parametricpinn/calibration/bayesianinference/likelihoods/likelihoods.py
@@ -17,6 +17,7 @@ from parametricpinn.calibration.bayesianinference.likelihoods.likelihoodstrategi
 )
 from parametricpinn.calibration.bayesianinference.likelihoods.optimization import (
     optimize_likelihood_hyperparameters_independently,
+    optimize_likelihood_hyperparameters,
     save_optimized_likelihood_hyperparameters,
 )
 from parametricpinn.calibration.bayesianinference.likelihoods.residualcalculator import (
@@ -533,7 +534,7 @@ def _create_optimized_noise_and_model_error_gps_likelihood_strategy(
         device=device,
     )
     likelihood_strategy.train()
-    optimize_likelihood_hyperparameters_independently(
+    optimize_likelihood_hyperparameters(
         likelihood=likelihood_strategy,
         prior_material_parameters=prior_material_parameters,
         num_material_parameter_samples=num_material_parameter_samples,
