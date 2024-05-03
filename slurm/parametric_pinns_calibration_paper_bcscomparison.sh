f03c81071f5722b3439667346a81447c3714cf99
David Anton <d.anton@tu-braunschweig.de>
2024-05-03 09:04:03 +0000

    Fix BCs comparison

--- slurm/parametric_pinns_calibration_paper_bcscomparison.sh
+++ slurm/parametric_pinns_calibration_paper_bcscomparison.sh
@@ -9,7 +9,7 @@
 ## Build command
 ## singularity build --fakeroot --force parametricpinn.sif app/.devcontainer/container_conda.def
 
-SCRIPT=parametric_pinn_2d_linearelasticity_quarterplatewithhole_bccomparison.py
+SCRIPT=parametric_pinns_calibration_paper_bcscomparison.py
 
 srun singularity run \
  --cleanenv \
