c23a8f20600133dd8a999b3d449ec3bc7a64e6da
David Anton <d.anton@tu-braunschweig.de>
2024-05-27 16:25:01 +0000

    Plot

--- parametric_pinns_calibration_paper_experimental_linearelasticity.py
+++ parametric_pinns_calibration_paper_experimental_linearelasticity.py
@@ -648,9 +648,9 @@ def calibration_step() -> None:
                     self.ticks_max_number_of_intervals = 255
                     self.num_cbar_ticks = 7
                     # resolution of results
-                    self.num_points_per_edge = 128
+                    self.num_points_per_edge = 512
                     # grid interpolation
-                    self.interpolation_method = "nearest"
+                    self.interpolation_method = "linear"
                     # save options
                     self.dpi = 300
                     self.file_format = "pdf"
@@ -676,8 +676,8 @@ def calibration_step() -> None:
                 max_displacement = np.nanmax(displacements)
                 
                 if dimension == "y":
-                    min_displacement = -0.005
-                    max_displacement = 0.005
+                    min_displacement = -0.01
+                    max_displacement = 0.01
 
                 # grid data
                 num_points_per_grid_dim = 128
